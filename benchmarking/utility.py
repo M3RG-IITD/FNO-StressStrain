6f8d212a8a12c39edb6299f99ffed4a781fadce4
meermehran <meermehran01@gmail.com>
2022-09-30 23:20:25 +0530

    update

---
+++ benchmarking/utility.py
@@ -0,0 +1,138 @@
+### @meermehran || M3RG Lab || IIT Delhi
+
+import torch
+import numpy as np
+import scipy.io
+import h5py
+import torch.nn as nn
+
+import operator
+from functools import reduce
+from functools import partial
+
+#################################################
+#
+# Utilities
+#
+#################################################
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+### MAT FILES ----->>>> Tensor Objects
+class MatReader(object):
+    def __init__(self, file_path, to_torch=True, to_cuda=False, to_float=True):
+        super(MatReader, self).__init__()
+
+        self.to_torch = to_torch
+        self.to_cuda = to_cuda
+        self.to_float = to_float
+
+        self.file_path = file_path
+
+        self.data = None
+        self.old_mat = None
+        self._load_file()
+
+    def _load_file(self):
+        try:
+            self.data = scipy.io.loadmat(self.file_path)
+            self.old_mat = True
+        except:
+            self.data = h5py.File(self.file_path)
+            self.old_mat = False
+
+    def load_file(self, file_path):
+        self.file_path = file_path
+        self._load_file()
+
+    def read_field(self, field):
+        x = self.data[field]
+
+        if not self.old_mat:
+            x = x[()]
+            x = np.transpose(x, axes=range(len(x.shape) - 1, -1, -1))
+
+        if self.to_float:
+            x = x.astype(np.float32)
+
+        if self.to_torch:
+            x = torch.from_numpy(x)
+
+            if self.to_cuda:
+                x = x.cuda()
+
+        return x
+
+    def set_cuda(self, to_cuda):
+        self.to_cuda = to_cuda
+
+    def set_torch(self, to_torch):
+        self.to_torch = to_torch
+
+    def set_float(self, to_float):
+        self.to_float = to_float
+
+# normalization, pointwise gaussian
+
+class UnitGaussianNormalizer(object):
+    def __init__(self, x, eps=0.00001):
+        super(UnitGaussianNormalizer, self).__init__()
+
+        # x could be in shape of ntrain*n or ntrain*T*n or ntrain*n*T
+        self.mean = torch.mean(x, 0)
+        self.std = torch.std(x, 0)
+        self.eps = eps
+
+    def encode(self, x):
+        x = (x - self.mean) / (self.std + self.eps)
+        return x
+
+    def decode(self, x, sample_idx=None):
+        if sample_idx is None:
+            std = self.std + self.eps # n
+            mean = self.mean
+        else:
+            if len(self.mean.shape) == len(sample_idx[0].shape):
+                std = self.std[sample_idx] + self.eps  # batch*n
+                mean = self.mean[sample_idx]
+            if len(self.mean.shape) > len(sample_idx[0].shape):
+                std = self.std[:,sample_idx]+ self.eps # T*batch*n
+                mean = self.mean[:,sample_idx]
+
+        # x is in shape of batch*n or T*batch*n
+        x = (x * std) + mean
+        return x
+
+    def cuda(self):
+        self.mean = self.mean.cuda()
+        self.std = self.std.cuda()
+
+    def cpu(self):
+        self.mean = self.mean.cpu()
+        self.std = self.std.cpu()
+
+
+def toTensor(array):
+    '''Converts a numpy array to a torch tensor.'''
+    x = torch.from_numpy(array).type(torch.FloatTensor)
+    x.requires_grad = True
+    return x
+
+
+## Loss Function
+def lossfunc(ytrue, ypred):
+    batch = ytrue.size()[0]
+
+    diff = torch.norm(ypred.reshape(batch,-1) - ytrue.reshape(batch,-1), 2,1)
+    ynorm  = torch.norm(ytrue.reshape(batch,-1), 2,1)
+    
+    err= torch.sum(diff/ynorm)
+    
+    return err
+
+
+# print the number of parameters
+def count_params(model):
+    c = 0
+    for p in list(model.parameters()):
+        c += reduce(operator.mul, list(p.size()))
+    return c
